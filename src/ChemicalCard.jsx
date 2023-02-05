d79c8bcb763e3ba70e97bb02c320962348fc2aaf
Ant <anthony.hillairet@gmail.com>
2023-02-05 08:24:14 -0800

    A first version with static data

---
+++ src/ChemicalCard.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Card } from 'primereact/card';
+import { Button } from 'primereact/button';
+
+import { ActivityList } from './ActivityList';
+
+import "primeflex/primeflex.css";                                  //easy CSS flex
+
+export const ChemicalCard = ({ chemical, description, activities, color }) => {
+
+  return (
+    <Card title={chemical} subTitle={description} style={{ backgroundColor: color }} >
+      <div class="flex justify-content-start">
+          <ActivityList activities={activities} />
+      </div>
+    </Card>
+  )
+}
