911b4297a6291e983a0a94b640410fd423f61945
Ant <anthony.hillairet@gmail.com>
2023-04-23 07:55:48 -0700

    Remove comment that was rendered

--- src/ActivityList.jsx
+++ src/ActivityList.jsx
@@ -1,7 +1,6 @@
 export const ActivityList = ({ activities }) => {
   return (
     <ul>
-      // Here I assume `activity` is a unique string & can be used as key
       {activities.map((activity) => (
         <li key={activity}>{activity}</li>
       ))}
